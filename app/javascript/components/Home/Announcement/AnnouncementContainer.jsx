b8077a6635156ee9783dc1cf9a65ca1a855ead4f
Tony Phu <tphu118@gmail.com>
2019-11-27 23:19:56 -0500

    made edit button connect to server

--- app/javascript/components/Home/Announcement/AnnouncementContainer.jsx
+++ app/javascript/components/Home/Announcement/AnnouncementContainer.jsx
@@ -5,8 +5,62 @@ class AnnouncementContainer extends Component {
         super(props)
         this.state = {
             announcementData: [],
-            eventData: []
+            eventData: [],
+            hideEditAnnounce: true,
+            description: "",
+            flier: "",
+            refreshKey: false,
         }
+
+        this.clickEdit = this.clickEdit.bind(this)
+        this.onChange = this.onChange.bind(this)
+        this.onSubmit = this.onSubmit.bind(this)
+    }
+
+    clickEdit(event) {
+        if (this.state.hideEditAnnounce === false) {
+            this.setState ({ hideEditAnnounce : true})
+        } else {
+            this.setState ({ hideEditAnnounce : false})
+        }
+    }
+
+    onChange(event) {
+        this.setState ({ [event.target.name]: event.target.value })
+    }
+
+    onSubmit(event) {
+        event.preventDefault()
+        // need to make url more dynamic than hard code 1
+        const urls = "/api/v1/announcements/1"
+        const { description, flier } = this.state
+
+        const body = {
+            description, flier
+        }
+
+        const token = document.querySelector('meta[name="csrf-token"]').content
+
+        fetch(urls, {
+            method: "PUT",
+            headers: {
+                "X-CSRF-Token": token,
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(body)
+        })
+        .then (response => {
+            if (response.ok) {
+                return response
+            } else {
+                let errorMessage = `${response.status} (${response.statusText})`,
+                error = new Error(errorMessage)
+                throw error
+            }
+        })
+        .then(this.setState ({ refreshKey: true }))
+        .catch(error => console.log(error.message)
+        )
     }
 
     // componentDidMount(){
@@ -35,15 +89,45 @@ class AnnouncementContainer extends Component {
             return Promise.all([response1.json(), response2.json()])
             })
         .then(([response1, response2]) => {
-            this.setState ({ announcementData : response1 })
-            this.setState ({ eventData : response2 })
+            this.setState ({ 
+                announcementData : response1, 
+                description : response1[0].description, 
+                flier : response1[0].flier })
+            this.setState ({ eventData : response2 })    
         })
+        // need to add error messages
         .catch(error => console.log(error.message)
         )
     }
 
+    componentDidUpdate() {
+        if (this.state.refreshKey === true) {
+            Promise.all([fetch("/api/v1/announcements"), fetch("/api/v1/events")])
+            .then(([response1, response2]) => {
+                return Promise.all([response1.json(), response2.json()])
+                })
+            .then(([response1, response2]) => {
+                this.setState ({ 
+                    announcementData : response1, 
+                    description : response1[0].description, 
+                    flier : response1[0].flier })
+                this.setState ({ eventData : response2 })
+            })
+            .then(this.setState ({ refreshKey : false }))
+            // need to add error messages
+            .catch(error => console.log(error.message)
+            )
+        }
+    }
+
     render() {
-        console.log(this.state);
+
+        let hide
+        if (this.state.hideEditAnnounce === true) {
+            hide = "invisible"
+        } else {
+            hide = ""
+        }
         
         let announcementData = this.state.announcementData
         let eventData = this.state.eventData
@@ -58,7 +142,7 @@ class AnnouncementContainer extends Component {
 
         let event = eventData.map(element => {
             return (
-            <li>
+            <li key={element.id}>
                 {element.title} - {element.location} - {element.date} 
             </li>
             )
@@ -72,6 +156,38 @@ class AnnouncementContainer extends Component {
                     </div>
                     <div className="pb-5">
                         <p>{announcementDescription}</p>
+                    </div>
+                    <button type="button" className="btn btn-info" onClick={this.clickEdit}>Edit</button>
+                    <div className={"container" + " " + hide}>
+                        <div className="row">
+                            <div className="col-xs-12 col-sm-12 col-md-12">
+                `               <form onSubmit={this.onSubmit}>
+                                    <div className="form-group">
+                                        <input 
+                                            type="text"
+                                            name="description"
+                                            id="description"
+                                            className="form-control"
+                                            onChange={this.onChange}
+                                            placeholder={this.state.description}
+                                        />
+                                    </div>
+                                    <div className="form-group">
+                                        <input 
+                                            type="text"
+                                            name="flier"
+                                            id="flier"
+                                            className="form-control"
+                                            onChange={this.onChange}
+                                            placeholder={this.state.flier}
+                                        />
+                                    </div>
+                                    <button type="submit" className="btn custom-button">
+                                        Submit Change
+                                    </button>
+                                </form>
+                            </div>
+                        </div>
                     </div>
                     <div className="row">
                         <div className="col-sm-6" style={{ height: "100%" }}>
