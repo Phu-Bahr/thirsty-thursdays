afa362e308f7db382dd1813a5e2a5fa8da6fe073
Tony Phu <tphu118@gmail.com>
2019-12-04 16:33:27 -0500

    took out borders, created max height, need consistent images for test.

--- app/javascript/components/Home/Venue/VenueTile.jsx
+++ app/javascript/components/Home/Venue/VenueTile.jsx
@@ -3,16 +3,20 @@ import React from 'react'
 const VenueTile = props => {     
   
   return(
-    <div className="col-md-6 border">
-      <img style={{width: '300px', height: '200px'}} src={props.venueImage}/>
-      <div>{props.name}</div>
-      <div>{props.street}</div>
-      <div>{props.city}, {props.state} {props.zip}</div>
-      <div>{props.telephone}</div>
-      <div>{props.url}</div>
-      <button type="button" className="btn btn-secondary" onClick={props.handleClick}>
-        Delete Venue
-      </button>
+    <div className="col-md-6 col-middle p-3">
+      <div>
+        <img src={props.venueImage} />
+      </div>
+      <div className="pt-3">
+        <div>{props.name}</div>
+        <div>{props.street}</div>
+        <div>{props.city}, {props.state} {props.zip}</div>
+        <div>{props.telephone}</div>
+        <div>{props.url}</div>
+        <button type="button" className="btn btn-secondary" onClick={props.handleClick}>
+          Delete Venue
+        </button>
+      </div>
     </div>
   )
 }
